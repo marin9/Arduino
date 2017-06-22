646261699014d14b7c713be1866dfa02cf77a1d6
marin <marin.bralic9@gmail.com>
2017-06-22 13:39:27 +0200

    Upload

---
+++ rtc.ino
@@ -0,0 +1,55 @@
+#include <Wire.h> 
+
+void setup(void) { 
+  Wire.begin();
+  Serial.begin(9600);  
+} 
+
+void loop(void) { 
+  Wire.beginTransmission(0x68);
+  Wire.write(0x00);    
+  Wire.write(0x00);
+  Wire.write(0x00);
+  Wire.endTransmission();
+
+  while(1){  
+    Serial.print("Minute: ");
+    Serial.println(readMinute(), DEC);
+    Serial.print("Seconds: ");
+    Serial.println(readSeconds(), DEC);
+    Serial.println("");
+    delay(1000);
+  }
+} 
+
+char readSeconds(){
+  char reg, sec;
+  Wire.beginTransmission(0x68);
+  Wire.write(0x00);  
+  Wire.endTransmission();
+
+  Wire.requestFrom(0x68, 1);  
+  while(Wire.available()){ 
+    reg=Wire.read();           
+  }
+
+  sec=10*((reg&0x70)>>4)+(reg&0x0F);
+  return sec;
+}
+
+char readMinute(){
+  char reg, min;
+  Wire.beginTransmission(0x68);
+  Wire.write(0x01);  
+  Wire.endTransmission();
+
+  Wire.requestFrom(0x68, 1);  
+  while(Wire.available()){ 
+    reg=Wire.read();           
+  }
+
+  min=10*((reg&0x70)>>4)+(reg&0x0F);
+  return min;
+}
+
+
---
+++ snd.ino
@@ -0,0 +1,15 @@
+void setup() {
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  char d=5;
+  while(1){
+    digitalWrite(13, HIGH);
+    delay(d);
+    digitalWrite(13, LOW);
+    delay(d);
+
+    
+  }
+}
